26ee755fe2ff36901dbff9378eaa54da1c106c80
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-09-11 11:42:13 -0700

    added env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
 import "./App.css";
-import Practice from "./components/Practice";
-import { firstInterface } from "./interfaces";
 
 function App() {
-  function testFunction(variable: firstInterface) {
-    console.log("first", variable);
-  }
-
-  const myVar: firstInterface = {
-    greeting: "hello",
-  };
-
-  const myNewVar: firstInterface = {
-    greeting: "goodby",
-  };
-
-  testFunction(myVar);
-
-  return (
-    <div>
-      <Practice myVar={myVar} myNewVar={myNewVar} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
